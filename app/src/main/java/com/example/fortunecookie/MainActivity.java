f1fcdee585ea823dc342b1fbbb85e08e347b0845
Gursimir <kgursimran648@gmail.com>
2020-12-04 23:44:51 +0530

    commit

--- app/src/main/java/com/example/fortunecookie/MainActivity.java
+++ app/src/main/java/com/example/fortunecookie/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.fortunecookie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
+
+        String fortune[] = {"The love of your life is right in front of your eyes.",
+
+                "Behind this fortune is the love of my life.",
+
+                "You have a secret admirer.",
+
+                "Love, because it is the only true adventure.",
+
+                "The love of your life will appear in front of you unexpectedly!",
+
+                "An old love will come back to you.",
+
+                "Your love life will soon be happy and harmonious."};
+            Random random = new Random();
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    textView.setText(fortune[random.nextInt(fortune.length)]);
+                }
+            });
     }
 }
