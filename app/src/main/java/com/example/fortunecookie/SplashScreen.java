4ccf686c4b78163d052a55066c420c958cc2b00f
Gursimir <kgursimran648@gmail.com>
2020-12-04 23:49:27 +0530

    commit

--- app/src/main/java/com/example/fortunecookie/SplashScreen.java
+++ app/src/main/java/com/example/fortunecookie/SplashScreen.java
@@ -2,7 +2,10 @@ package com.example.fortunecookie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        final Intent i = new Intent(SplashScreen.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
     }
 }
